9158585223dd9d401723e31fc4350c4243f38c4e
byHYONS <byhyons@gmail.com>
2024-05-03 11:53:37 +0200

    first commit

---
+++ jsnack-2/main.js
@@ -0,0 +1,33 @@
+// reset 
+'use strict'
+
+console.log('CIAO!!!');
+
+// 1. dichiaro var con array vuoto;
+// 2. attraverso un prompt do la possibilità di inserire due parole all'utente;
+// 3. confronto le parole tramite `nomeVar.length`;
+// 4. mando a schermo prima la parola più corta e poi quella più lunga.
+
+const parole = [];
+const paroleIn1 = prompt('inserisci la prima parola');
+const paroleIn2 = prompt('inserisci la seconda parola');
+
+parole.push(paroleIn1);
+console.log({parole});
+
+parole.push(paroleIn2);
+console.log({parole});
+
+let trovaParola = false;
+
+for (let i = 0; i<parole.length; i++){
+    if (paroleIn1 > paroleIn2){
+        trovaParola = true;
+        
+        
+    } else if (paroleIn1 < paroleIn2){
+        
+
+    }
+    console.log(trovaParola ? `La parola piu lunga è ${paroleIn1}` : `La parola piu lunga è ${paroleIn2}`);
+}
---
+++ redme.md
@@ -0,0 +1,16 @@
+# ESERCIZIO: SNACKS
+
+>[!NOTE]
+>
+> Nome repo: js-jsnacks-blocco-1
+
+### JSnack 2:
+L’utente inserisce due parole in successione, con due prompt.
+Il software stampa prima la parola più corta, poi la parola più lunga.
+
+### Svolgimento:
+1. dichiaro var con array vuoto;
+2. attraverso un prompt do la possibilità di inserire due parole all'utente;
+3. confronto le parole tramite `nomeVar.length`;
+4. mando a schermo prima la parola più corta e poi quella più lunga.
+
