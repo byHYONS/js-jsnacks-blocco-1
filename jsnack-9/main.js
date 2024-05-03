4028795c9cdffde8e3911d210b02b1144e92f495
byHYONS <byhyons@gmail.com>
2024-05-03 17:16:20 +0200

    [FIT]

--- jsnack-9/main.js
+++ jsnack-9/main.js
@@ -1,31 +1,3 @@
 // reset
 'use strict'
 
-// 1. chiedo all'utente di inserire un numero di 4 cifre tramite un prompt;
-// 2. lascio il risultato in stringa (senza trasformarlo in numero);
-// 3. faccio una istruzione di iterazione che cicli per la lunghezza della stringa (cioè 4);
-// 4. faccio la somma per poi trasformare la stringa in numero.
-
-let numeroIn;
-let somma = 0;
-
-// cerco di fare inserire all'utente un numero di 4 cifre esatte:
-for (;;) {
-    numeroIn = prompt('Inserisci un numero di 4 cifre');
-    if (numeroIn.length === 4){
-        break
-    } else {
-        console.log(`Hai inserito un numero superiore o inferiore a 4 cifre. Inseriscilo nuovamente`);        
-    }
-}
-
-// faccio la somma delle cifre:
-for (let i = 0; i<numeroIn.length; i++){   
-    somma += parseInt(numeroIn[i]);
-    console.log({somma});         
-}
-
-alert(`La somma delle 4 cifre da te inserite è ${somma}`);
-
-
- // FINE
