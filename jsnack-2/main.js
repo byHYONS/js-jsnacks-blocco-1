65e9136b327a166f295bab0c114cda029b454636
byHYONS <byhyons@gmail.com>
2024-05-03 11:57:16 +0200

    [FIT]

--- jsnack-2/main.js
+++ jsnack-2/main.js
@@ -13,20 +13,17 @@ const paroleIn1 = prompt('inserisci la prima parola');
 const paroleIn2 = prompt('inserisci la seconda parola');
 
 parole.push(paroleIn1);
-console.log({parole});
+console.log({paroleIn1});
 
 parole.push(paroleIn2);
-console.log({parole});
+console.log({paroleIn2});
 
 let trovaParola = false;
 
 for (let i = 0; i<parole.length; i++){
     if (paroleIn1 > paroleIn2){
-        trovaParola = true;
-           
+        trovaParola = true;           
     } 
-
-
     
     console.log(trovaParola ? `La parola piu lunga è ${paroleIn2}` : `La parola piu lunga è ${paroleIn1}`);
 }
